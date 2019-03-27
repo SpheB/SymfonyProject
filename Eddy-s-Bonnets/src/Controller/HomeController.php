e61597c76261fd1a1a8a3ad4e01f5e3e62dc0d56
Sophie <sphe.bonnet@gmail.com>
2019-03-27 10:16:42 +0100

    views liées à home dans template et entity:auth faite

--- Eddy-s-Bonnets/src/Controller/HomeController.php
+++ Eddy-s-Bonnets/src/Controller/HomeController.php
@@ -8,11 +8,53 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+     /**
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return $this->render('home/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+    //peut être autre controller pour looks???
+     /**
+     * @Route("/looks", name="looks")
+     */
+    public function looks()
+    {
+        return $this->render('home/looks.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+     //si j'ai le temps???
+     /**
+     * @Route("/news", name="news")
+     */
+    public function news()
+    {
+        return $this->render('home/news.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('home/contact.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
---
+++ Eddy-s-Bonnets/src/Entity/News.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text_news;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_news;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture_news;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getTextNews(): ?string
+    {
+        return $this->text_news;
+    }
+
+    public function setTextNews(string $news_text): self
+    {
+        $this->text_news = $news_text;
+
+        return $this;
+    }
+
+    public function getDateNews(): ?\DateTimeInterface
+    {
+        return $this->date_news;
+    }
+
+    public function setDateNews(?\DateTimeInterface $news_date): self
+    {
+        $this->date_news = $news_date;
+
+        return $this;
+    }
+
+    public function getPictureNews(): ?string
+    {
+        return $this->picture_news;
+    }
+
+    public function setPictureNews(?string $picture_news): self
+    {
+        $this->picture_news = $picture_news;
+
+        return $this;
+    }
+}
